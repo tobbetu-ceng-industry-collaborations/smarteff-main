6d379a8776108ecaa55dea0a990b7f17fdc95485
BaturayOnural <baturayonural@gmail.com>
2020-01-26 17:04:32 +0300

    Added Device turnon/turnoff event

--- app/controllers.py
+++ app/controllers.py
@@ -36,3 +36,30 @@ def person_event():
 
     return resp, 200
 
+# endpoint to handle device events
+@app.route("/HandleDeviceEvent", methods=['POST'])
+def device_event():
+
+    # parse http request body
+    data = request.json
+    device_id= data['deviceid']
+    event = data['event']
+
+    # get person or throw 404
+    device = Device.query.get_or_404(device_id)
+
+    # change is_inside status according to event
+    if event == 'turnon':
+        device.is_on = 1;
+    elif event == 'turnoff':
+        device.is_on = 0;
+
+    # write changes to DB
+    db.session.add(device)
+    db.session.commit()
+
+    # prepare the response --> assuming everything is OK
+    resp = jsonify({'success':True})
+
+    return resp, 200
+
