daa17a7f5b5d55ad151f8e06b35ff28166573f3b
denizonerr <43685845+denizonerr@users.noreply.github.com>
2020-01-27 23:54:09 +0300

    Add files via upload

---
+++ WebApplication10/Controllers/DeviceController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class DeviceController : Controller
+    {
+        // GET: Device
+        IList<Device> DeviceList = new List<Device>()
+        {new Device() { DeviceId = 11, status = "on", automation = "on",ownerId=2 } ,
+                            new Device() { DeviceId = 21, status = "off",  automation = "on", ownerId=1 } ,
+                            new Device() { DeviceId = 31, status = "on",  automation = "off", ownerId=2  } ,
+                            new Device() { DeviceId = 41, status = "on" , automation = "on" ,ownerId=1 } , };
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult devicereturn(int Id)
+        {
+            //Get the student from studentList sample collection for demo purpose.
+            //Get the student from the database in the real application
+            var std = DeviceList.Where(s => s.ownerId == Id).FirstOrDefault();
+
+            return View(std);
+        }
+    }
+}
---
+++ WebApplication10/Controllers/PersonController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication10.Models;
+
+namespace WebApplication10.Controllers
+{
+    public class PersonController : Controller
+
+
+    {
+
+        IList<Person> PersonList = new List<Person>()
+        {new Person() { PersonId = 1, PersonName = "John", Age = 18 } ,
+                            new Person() { PersonId = 2, PersonName = "Steve",  Age = 21 } ,
+                            new Person() { PersonId = 3, PersonName = "Bill",  Age = 25 } ,
+                            new Person() { PersonId = 4, PersonName = "Ram" , Age = 20 } , };
+        // GET: Person
+        public ActionResult Index()
+        {
+            var PersonList = new List<Person>{
+                            new Person() { PersonId = 1, PersonName = "John", Age = 18 } ,
+                            new Person() { PersonId = 2, PersonName = "Steve",  Age = 21 } ,
+                            new Person() { PersonId = 3, PersonName = "Bill",  Age = 25 } ,
+                            new Person() { PersonId = 4, PersonName = "Ram" , Age = 20 } ,
+                             
+                        };
+            return View(PersonList);
+        }
+
+        [ActionName("find")]
+        public ActionResult GetById(int id)
+        {
+            // get student from the database 
+            return View();
+        }
+
+
+        public ActionResult Login(int Id)
+        {
+            //Get the student from studentList sample collection for demo purpose.
+            //Get the student from the database in the real application
+            var std = PersonList.Where(s => s.PersonId == Id).FirstOrDefault();
+
+            return View(std);
+        }
+    }
+}
