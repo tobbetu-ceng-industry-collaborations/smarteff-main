6293877ee60e6781027761f0ea848e5e4797badb
denizonerr <43685845+denizonerr@users.noreply.github.com>
2020-03-08 00:54:46 +0300

    Update HomeController.cs

--- WebApplication10/Controllers/HomeController.cs
+++ WebApplication10/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebApplication11.Controllers
+namespace WebApplication10.Controllers
 {
     public class HomeController : Controller
     {
