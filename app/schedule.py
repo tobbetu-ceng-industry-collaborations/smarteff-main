4f47bda25ea8188bf77098bdb313e1e081a8768d
BaturayOnural <baturayonural@gmail.com>
2020-03-17 21:55:57 +0300

    Heroku Bugfix

--- app/schedule.py
+++ app/schedule.py
@@ -10,13 +10,13 @@ s = sched.scheduler(time.time, time.sleep)
 
 # request a device to be turned off with its given id
 def request_device_off(dev_id):
-	os.system(("bash /test_api/POST/request_device_off.sh " +  str(dev_id)).format())
+	os.system(("bash test_api/Heroku/POST/request_device_off.sh " +  str(dev_id)).format())
 
 # turn device off with its given id
 def device_off(dev_id):
 
 	# make call from test_api
-	os.system(("bash /test_api/POST/device_off.sh " +  str(dev_id)).format())
+	os.system(("bash test_api/Heroku/POST/device_off.sh " +  str(dev_id)).format())
 
 # general scheduler
 def schedule(device_id, until_suspension):
