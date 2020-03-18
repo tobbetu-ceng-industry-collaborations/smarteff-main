9c89eb2337cfc34e1f73c26be1d9948d97a97ba5
BaturayOnural <baturayonural@gmail.com>
2020-03-18 14:30:53 +0300

    Added tests for the new endpoint

---
+++ test_api/Heroku/POST/remove_shutdown.sh
@@ -0,0 +1,1 @@
+curl  -H "Content-Type: application/json" -X POST https://smarteff.herokuapp.com/RemoveScheduledShutdown/"$1"
---
+++ test_api/POST/remove_shutdown.sh
@@ -0,0 +1,1 @@
+curl  -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/RemoveScheduledShutdown/"$1"
