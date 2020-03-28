e97ec84b9516ff3d664db84a3cf7cfa3a43adacc
BaturayOnural <baturayonural@gmail.com>
2020-03-28 14:17:51 +0300

    Added a new simulation view for #14

--- app/views.py
+++ app/views.py
@@ -44,3 +44,34 @@ def admin():
 
     return render_template("office.html", persons=persons, devices=devices, device_status=device_status, loaded=loaded)
 
+# simulation view 
+@app.route("/simulate", methods=['POST','GET'])
+def simulate():
+
+    # get person list as json
+    response = requests.get("http://0.0.0.0:5000/ListPersons")
+    response = response.text
+    loaded = json.loads(response)
+
+    # get person-device assignment as json
+    response2 = requests.get("http://0.0.0.0:5000/ListPersonDeviceAssignment")
+    response2 = response2.text
+    devices_temp = json.loads(response2)
+
+    # reverse dictionary
+    devices = {v: k for k, v in devices_temp.items()}
+
+    # get device status as json
+    response3 = requests.get("http://0.0.0.0:5000/ListDeviceStatus")
+    response3 = response3.text
+    device_status = json.loads(response3)
+
+    # person array to redirect
+    persons = []
+
+    # append person names to the list
+    for person in loaded['people']:
+        persons.append(person['name'])
+
+    return render_template("simulation.html", persons=persons, devices=devices, device_status=device_status, loaded=loaded)
+
